8473b62733c5f2712ac66d072a56315a4bc3fae4
srinidhi-m-s <140908806+the-bluemoon@users.noreply.github.com>
2025-10-17 02:55:43 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect to MongoDB
